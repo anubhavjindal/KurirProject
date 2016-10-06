4a1f561827b4abf78f85000c4b923b3c898b7a5b
bhoomikagupta <bhoomika.gupta.08@gmail.com>
2016-10-06 17:58:42 +0530

    Message.java

---
+++ app/src/main/java/com/anubhavj/kurir/services/entities/Message.java
@@ -0,0 +1,109 @@
+package com.anubhavj.kurir.services.entities;
+
+/**
+ * Created by Bhoomika_2 on 06-10-2016.
+ */
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Calendar;
+
+public class Message implements Parcelable {
+    private int id;
+    private Calendar createdAt;
+    private String shortMessage;
+    private String longMessage;
+    private String imageUrl;
+    private UserDetails otherUser;
+    private boolean isFromUs;
+    private boolean isRead;
+    private boolean isSelected;
+
+
+    public Message(int id,
+                   Calendar createdAt,
+                   String shortMessage,
+
+                   String longMessage,
+                   String imageUrl,
+                   UserDetails otherUser,
+                   boolean isFromUs,
+                   boolean isRead) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.shortMessage = shortMessage;
+        this.longMessage = longMessage;
+        this.imageUrl = imageUrl;
+        this.otherUser = otherUser;
+        this.isFromUs = isFromUs;
+        this.isRead = isRead;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public Calendar getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getShortMessage() {
+        return shortMessage;
+    }
+
+    public String getLongMessage() {
+        return longMessage;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public UserDetails getOtherUser() {
+        return otherUser;
+    }
+
+    public boolean isFromUs() {
+        return isFromUs;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setIsSelected(boolean isSelected) {
+        this.isSelected = isSelected;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+
+    public static final Creator<Message> CREATOR = new Creator<Message>() {
+
+        public Message createFromParcel(Parcel source) {
+            return null;
+
+        }
+
+        @Override
+        public Message[] newArray(int size) {
+            return new Message[0];
+        }
+    };
+}
+
+
+
+
